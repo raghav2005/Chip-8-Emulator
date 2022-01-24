67c64ef1ad4c30eb529603213c73732b8e5d019c
raghav2005 <raghavawasthi2005@gmail.com>
2022-01-24 20:39:31 +0400

    changed scale size for frontend

--- desktop_frontend/src/main.rs
+++ desktop_frontend/src/main.rs
@@ -4,7 +4,7 @@ use sdl2;
 use std::io::Read;
 
 // scale up 64x32 monitor
-const SCALE_SIZE: u32 = 12;
+const SCALE_SIZE: u32 = 21;
 // actual window width
 const WINDOW_WIDTH: u32 = (backend::SCREEN_WIDTH as u32) * SCALE_SIZE;
 // actual window height
